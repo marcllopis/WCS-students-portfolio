7df9ea8632277492f6b2309644b9705107b68cbe
mireiaGomariz <mireiapgomariz@gmail.com>
2020-04-16 12:35:50 +0200

    change campus name on the text file

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,18 +2,23 @@ import React from 'react';
 import logo from './wildCodeLogo.png';
 import spainFlag from './spainFlag2.svg';
 import contactIcon from './contactIcon.png';
+import { Link } from 'react-router-dom';
+import { Context } from '../../Context/Provider';
 import './header.css';
 
 
 const Header = () => {
+  const { state } = React.useContext(Context);
   return (
     <div className="titleContainer">
       <img alt="logo WCS" src={logo} className="logoWCS" />
-      <h3 className="campusName">Barcelona Campus</h3>
+      <h3 className="campusName">{state[1].campusName}</h3>
       <div className="countryContact">
         <img alt="Spain" src={spainFlag} className="countryFlag" />
-        <img alt="Contact Icon" src={contactIcon} className="contactIcon" />
-
+        <Link
+          className="contactIcon">
+          <img alt="Contact Icon" src={contactIcon}/>
+        </Link>
       </div>
     </div>
   );
