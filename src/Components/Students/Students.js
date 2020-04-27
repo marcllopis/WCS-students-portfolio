8f19328cbd5411b81b99cac17253beaaafb5f39d
Marc <marcllopis89@gmail.com>
2020-04-27 09:57:42 +0200

    Adds main styling for student page and also:  - Refactors footer and navbar  - Adds new flags and the logic to display a flag with a given text  - Adds main styling for the student profile page

--- src/Components/Students/Students.js
+++ src/Components/Students/Students.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import shortId from 'shortid';
+import YouTube from 'react-youtube';
 
 import Header from '../Header/header.js';
 import Footer from '../Footer/Footer.js';
 import { Context } from '../../Context/Provider';
+import { Flags } from '../../Utils/Flags';
+import './Students.css';
 
+// const onReady = (event) => {
+//   // access to player in all event handlers via event.target
+//   event.target.pauseVideo();
+// };
 
 const Students = () => {
   const { state } = React.useContext(Context);
@@ -17,50 +24,90 @@ const Students = () => {
   const myStudent = state.currentStudent || state.students
     .filter((student) => student.english.name === studentNameInUrl)[0];
 
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
-    <div>
+    <>
       {myStudent ?
-        <div>
+        <>
           <Header />
-          <h2>Student profile</h2>
-          <p>Name: {myStudent[state.language].name}</p>
-          <p>Nationality: {myStudent[state.language].from}</p>
-          <p>Living In: {myStudent[state.language].livingIn}</p>
-          <p>Date of Birth: {myStudent[state.language].dateOfBirth}</p>
-          <p>Linkedin {myStudent[state.language].linkedin}</p>
-          <p>Languages: {myStudent[state.language].languages}</p>
-          <p>Long Description: {myStudent[state.language].longDescription}</p>
-          <p>Github Profile: {myStudent[state.language].github}</p>
-          <div>{myStudent[state.language].technologies.map((technology) => (
-            <p key={shortId.generate()}>{technology}</p>
-          ))}</div>
-          <div>Personal Projects:
-            {myStudent[state.language].personalProjects.map((project) => (
-              <div key={shortId.generate()}>
-                <p>{project.projectName}</p>
-                <p>{project.projectGithub}</p>
-                <p>{project.projectLink}</p>
-                <p>{project.projectDescription}</p>
-                <div>{project.collaborators.map((collaborator) => (
-                  <p key={shortId.generate()}>{collaborator}</p>
-                ))}</div>
-                <div>{project.technologies.map((technology) => (
-                  <p key={shortId.generate()}>{technology}</p>
-                ))}
+          <div className="student-profile-container">
+            <div className="profile-header">
+              <div className="profile-info-container">
+                <div className="profile-info">
+                  <div className="profile-image-container">
+                    <img className="profile-image" src={myStudent.english.profilePicture} alt="profile" />
+                  </div>
+                  <div className="profile-center">
+                    <p className="name">{myStudent[state.language].name}</p>
+                    <p className="from-living">From <span className="from-living-city">{myStudent[state.language].from}</span></p>
+                    <p className="from-living">Living in <span className="from-living-city">{myStudent[state.language].livingIn}</span></p>
+                  </div>
+                  <div className="profile-right">
+                    {myStudent[state.language].languages.map((language) => <img className="language-image" src={Flags[language]} alt="language" />)}
+                    <p className="birth-date">{myStudent[state.language].dateOfBirth}</p>
+                  </div>
                 </div>
               </div>
-            ))}
+            </div>
           </div>
-          <p>Video: {myStudent[state.language].video}</p>
-          <p>Social Network: {myStudent[state.language].socialNetwork}</p>
-          <p>Video: {myStudent[state.language].video}</p>
-          <p>Calendly: {myStudent[state.language].calendly}</p>
+          <div className="main-student-container">
+            <div>
+              <div className="main-student-info">
+                <h2 className="header-title">About me</h2>
+                <hr className="separator" />
+                {myStudent[state.language].longDescription.map((paragraph) =>
+                  <p>{paragraph}</p>)}
+                <h2 className="header-title">Watch me</h2>
+                <hr className="separator" />
+                <YouTube videoId="4XAC1yuGLi4" opts={opts} onReady={(e) => e.target.pauseVideo()} />
+                <h2 className="header-title">Find me on</h2>
+                <hr className="separator" />
+
+                <p>Linkedin {myStudent[state.language].linkedin}</p>
+                <p>Github Profile: {myStudent[state.language].github}</p>
+                <p>Social Network: {myStudent[state.language].socialNetwork}</p>
+                <h2 className="header-title">My projects</h2>
+                <hr className="separator" />
+
+                {myStudent[state.language].personalProjects.map((project) => (
+                  <div key={shortId.generate()}>
+                    <p>{project.projectName}</p>
+                    <p>{project.projectGithub}</p>
+                    <p>{project.projectLink}</p>
+                    <p>{project.projectDescription}</p>
+                    <div>{project.collaborators.map((collaborator) => (
+                      <p key={shortId.generate()}>{collaborator}</p>
+                    ))}</div>
+                    <div>{project.technologies.map((technology) => (
+                      <p key={shortId.generate()}>{technology}</p>
+                    ))}
+                    </div>
+                  </div>
+                ))}
+
+                {myStudent[state.language].technologies.map((technology) => (
+                  <p key={shortId.generate()}>{technology}</p>
+                ))}
+
+                <p>Video: {myStudent[state.language].video}</p>
+                <p>Calendly: {myStudent[state.language].calendly}</p>
+              </div>
+            </div>
+
+          </div>
 
           <Footer />
-        </div>
+        </>
         :
         <Redirect to="/" /> /* if no student found, go back to home page */}
-    </div>
+    </>
   );
 };
 
---
+++ src/Utils/Flags.js
@@ -0,0 +1,12 @@
+import Spanish from './img/spainFlag.svg';
+import English from './img/englishFlag.svg';
+import German from './img/germanFlag.png';
+import France from './img/franceFlag.png';
+
+
+export const Flags = {
+  spanish: Spanish,
+  english: English,
+  german: German,
+  french: France,
+};
